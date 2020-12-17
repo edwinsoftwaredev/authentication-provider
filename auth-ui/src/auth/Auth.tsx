f69f0ae2ca1f7e9570c4a028cc7f578d48214da1
Edwin <edwin_m_paz@hotmail.com>
2020-12-17 12:19:21 -0600

    Add Auth Component

--- auth-ui/src/auth/Auth.tsx
+++ auth-ui/src/auth/Auth.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom'
 import Login from './login/Login';
+import AuthStyle from './Auth.module.scss';
 
 const Auth: React.FC = () => {
-    return (
-        <Switch>
-            <Route exact path='/auth/login'>
-                <div>
-                    <Login />
-                </div>
-            </Route>
-        </Switch>
-    );
+  return (
+    <div className={AuthStyle['auth-component']}>
+      <Switch>
+        <Route exact path='/auth/login'>
+            <Login />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default Auth;
