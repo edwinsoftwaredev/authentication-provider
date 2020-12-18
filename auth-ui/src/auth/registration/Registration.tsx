13a61fa4398d418b8adf9b4e571d3e90fa97c555
Edwin <edwin_m_paz@hotmail.com>
2020-12-17 18:17:49 -0600

    Add initial Registration component

---
+++ auth-ui/src/auth/registration/Registration.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RegistrationStyle from './Registration.module.scss';
+
+const Registration: React.FC = () => {
+    return (
+        <div className={RegistrationStyle['registration-component']}>
+            Registration works!
+        </div>
+    )
+}
+
+export default Registration;
