ed06aa94b4b3170198e5fac7cff00c7dff2e09b6
Edwin <edwin_m_paz@hotmail.com>
2020-12-17 18:18:46 -0600

    Add Registration component route to Auth component

--- auth-ui/src/auth/Auth.tsx
+++ auth-ui/src/auth/Auth.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import {Route, Switch} from 'react-router-dom'
+import React, { useEffect } from 'react';
+import {Route, Switch, useLocation} from 'react-router-dom'
 import Login from './login/Login';
 import AuthStyle from './Auth.module.scss';
+import Registration from './registration/Registration';
 
 const Auth: React.FC = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/auth') {
+      window.location.href = process.env.REACT_APP_KRATOS_SELF_SERVICE_LOGIN ?? '/'; 
+    }
+  }, [location.pathname]);
+
   return (
     <div className={AuthStyle['auth-component']}>
       <Switch>
         <Route exact path='/auth/login'>
-            <Login />
+          <Login />
+        </Route>
+        <Route exact path='/auth/registration'>
+          <Registration />
         </Route>
       </Switch>
     </div>
