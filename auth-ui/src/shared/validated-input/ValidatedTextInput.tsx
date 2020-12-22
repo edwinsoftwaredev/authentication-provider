6a1b171dfccb886c9c0c5789fb85f636ab97fa32
Edwin <edwin_m_paz@hotmail.com>
2020-12-21 19:43:05 -0600

    Add Validated text input Component

---
+++ auth-ui/src/shared/validated-input/ValidatedTextInput.tsx
@@ -0,0 +1,71 @@
+import React, {useEffect, useState} from 'react';
+import ValidatedTextInputStyle from './ValidatedTextInput.module.scss';
+
+const ValidatedTextInput: React.FC<IValidatedTextInput> = (props: any) => {
+
+  const [isTouched, setIsTouched] = useState(false);
+  const [isValid, setIsValid] = useState(false);
+  const [message, setMessage] = useState('');
+
+  /*const textHandler = (value: string) => {
+    setIsTouched(true);
+
+    const validState = props.isValid(value, (message: string) => {
+      setMessage(message);
+    });
+
+    setIsValid(validState);
+    validState ? props.value(value) : props.value('');
+  };*/
+
+  // after props changes this useEffect is going to be executed!
+  useEffect(() => {
+    props.initialState((message: string, isValid: boolean) => {
+      setMessage(message);
+      setIsValid(isValid);
+      setIsTouched(!!message);
+    }); 
+  }, [props]);
+
+
+  return (
+    <div
+      className={
+        ValidatedTextInputStyle['validated-input-text'] + ' ' +
+        (!isTouched ? '' : isValid ? ValidatedTextInputStyle['touched'] : ValidatedTextInputStyle['not-valid'])
+      }
+    >
+      <div
+        className={ValidatedTextInputStyle['message-bar']}>
+        <div className={ValidatedTextInputStyle['message']}>
+          <div>
+            {props.others.fieldname + ' ' + (message)}
+          </div>
+        </div>
+      </div>
+      <input
+        className={ValidatedTextInputStyle['input']}
+        type='text'
+        name={props.name}
+        onChange={
+          event => {
+            setIsValid(true);
+            setIsTouched(true);
+            setMessage('');
+          }
+        }
+        {...props.others}
+      />
+    </div>
+  );
+}
+
+export interface IValidatedTextInput {
+  value: (value: string) => void;
+  // isValid: (value: string, setMessage: (message: string) => void) => boolean;
+  initialState: (setInputState: (message: string, isValid: boolean) => void) => void;
+  name: string;
+  others: object;
+}
+
+export default ValidatedTextInput;
