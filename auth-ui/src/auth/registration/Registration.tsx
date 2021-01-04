0ec55006cddad4b33fc26752bc403d06fee1e8f1
Edwin <edwin_m_paz@hotmail.com>
2021-01-04 11:10:23 -0600

    Replace html buttons for Button components. Add Sign In Button and Alert Message

--- auth-ui/src/auth/registration/Registration.tsx
+++ auth-ui/src/auth/registration/Registration.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import RegistrationStyle from './Registration.module.scss';
+import style from './Registration.module.scss';
 import {Configuration, PublicApi, RegistrationFlow} from '@oryd/kratos-client';
 import { useLocation } from 'react-router-dom';
 import ValidatedTextInput from '../../shared/validated-input/ValidatedTextInput';
+import Button from '../../shared/button/button';
+import AlertMessage from '../../shared/alert-message/AlertMessage';
 
 const Registration: React.FC = () => {
   const [flowId, setFlowId] = useState<string>('');
@@ -12,6 +14,7 @@ const Registration: React.FC = () => {
   const [emailMessages, setEmailMessages] =  useState<(string | undefined)[] | undefined>();
   const [passwordMessages, setPasswordMessages] =  useState<(string | undefined)[] | undefined>();
   const [nameMessages, setNameMessages] =  useState<(string | undefined)[] | undefined>();
+  const [flowMessages, setFlowMessages] = useState<(string | undefined)[] | undefined>();
   const [action, setAction] = useState<string>('');
 
   const [username, setUsername] = useState<string>('');
@@ -19,6 +22,13 @@ const Registration: React.FC = () => {
   const [name, setName] = useState<string>('');
 
   const location = useLocation();
+
+  const handleSignInClick = () => {
+    const redir = process.env.REACT_APP_KRATOS_SELF_SERVICE_LOGIN;
+    if (redir) {
+      window.location.href = redir;
+    }
+  };
 
   useEffect(() => {
     setFlowId(new URLSearchParams(location.search).get('flow') ?? '');
@@ -107,20 +117,28 @@ const Registration: React.FC = () => {
           ?.messages
           ?.map(value => value.text)
       );
+      setFlowMessages(
+        registrationFlow
+          .methods
+          .password
+          .config
+          .messages
+          ?.map(message => message.text)
+      )
     }
   }, [registrationFlow])
 
   return (
-    <div className={RegistrationStyle['registration-component']}>
+    <div className={style['registration-component']}>
       <form 
-        className={RegistrationStyle['registration-form']}
+        className={style['registration-form']}
         action={action}
         method='POST'
         encType='application/x-www-urlencoded'
       >
-        <div className={RegistrationStyle['form-fields']}>
+        <div className={style['form-fields']}>
           <input 
-            className={RegistrationStyle['form-field']}
+            className={style['form-field']}
             type='hidden'
             name='csrf_token'
             value={csrfToken}
@@ -162,12 +180,22 @@ const Registration: React.FC = () => {
             name='password'
             others={{fieldname: 'Password', autoComplete: 'off', type: 'password'}}
           />
-          <button
-            className={RegistrationStyle['btn-submit']}
-            type='submit'
-          >
-            Sign Up
-          </button>
+          {
+            flowMessages ? <AlertMessage message={flowMessages.join(', ')} type={'error'}/> : null
+          }
+          <div className={style['auth-buttons']}>
+            <Button
+              classType={'default'}
+              text='Sign Up'
+              type='submit'
+            />
+            <Button
+              classType={'default'}
+              text='Do you already have an account?'
+              type='button'
+              onClick={handleSignInClick}
+            />
+          </div>
         </div>
       </form>
     </div>
