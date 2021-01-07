8122d3cbbd109bbdb9955b08d3cef2469cdd7974
Edwin <edwin_m_paz@hotmail.com>
2021-01-06 20:42:22 -0600

    Initialize blueprints and set env vars

--- auth-server/authserver/__init__.py
+++ auth-server/authserver/__init__.py
@@ -1,11 +1,15 @@
 import os
 
 from flask import Flask
+from . import blueprints
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    # app.config.from_mapping()
+    app.config.from_mapping(
+        WTF_CSRF_SECRET_KEY=os.environ.get('WTF_CSRF_SECRET_KEY'),
+        SECRET_KEY=os.environ.get('SECRET_KEY')
+    )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -13,16 +17,13 @@ def create_app(test_config=None):
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
-    
+
     # ensure the instance folder exists
-    try: 
+    try:
         os.mkdir(app.instance_path)
     except OSError:
         pass
 
-    @app.route('/hello')
-    def hello():
-        return 'Hello World!'
-
-
-    return app;
+    blueprints.init_app(app)
+    
+    return app
