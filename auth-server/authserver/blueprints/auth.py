e943715598b6c71b96487999510a8714e9a31cb8
Edwin <edwin_m_paz@hotmail.com>
2021-01-07 13:22:55 -0600

    Move CORS call to root __init__.py

--- auth-server/authserver/blueprints/auth.py
+++ auth-server/authserver/blueprints/auth.py
@@ -1,10 +1,7 @@
-import os
-
 from flask import Blueprint
 from flask_cors import CORS
 
 bp = Blueprint('auth', __name__, url_prefix='/api/auth')
-CORS(bp, origins=[os.environ.get('ALLOWED_ORIGIN')])
 
 @bp.route('/challenge', methods=['POST'])
 def login_challenge():
