aba7d32bd18ccfe2f1b3ef67e0945f3abf1e52e8
Edwin <edwin_m_paz@hotmail.com>
2021-01-07 13:23:58 -0600

    Change cookie name and move CORS call to root __init__.py

--- auth-server/authserver/blueprints/csrf_token.py
+++ auth-server/authserver/blueprints/csrf_token.py
@@ -1,7 +1,4 @@
-import os
-
 from flask import Blueprint
-from flask.globals import session
 from flask.helpers import make_response
 from flask.json import jsonify
 from flask.wrappers import Response
@@ -9,7 +6,6 @@ from flask_cors import CORS
 from flask_wtf.csrf import generate_csrf
 
 bp = Blueprint('csrf_token', __name__, url_prefix='/api/auth')
-CORS(bp, origins=[os.environ.get('ALLOWED_ORIGIN')])
 
 @bp.route('/csrf-token', methods=['GET'])
 def get_token():
@@ -18,5 +14,5 @@ def get_token():
 
 @bp.after_request
 def inject_csrf_token(response: Response):
-    response.set_cookie('XSRF-TOKEN', generate_csrf())
+    response.set_cookie('auth-csrf-token', value=generate_csrf())
     return response
