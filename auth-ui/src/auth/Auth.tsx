afa99684e951592676a139ea87eef93756fa68c7
Edwin <edwin_m_paz@hotmail.com>
2021-01-07 15:18:08 -0600

    Replace code for custom hooks

--- auth-ui/src/auth/Auth.tsx
+++ auth-ui/src/auth/Auth.tsx
@@ -4,55 +4,35 @@ import Login from './login/Login';
 import style from './Auth.module.scss';
 import Registration from './registration/Registration';
 import Verification from './verification/Verification';
-import Axios, { AxiosResponse } from 'axios';
+import Axios from 'axios';
+import useWhoAmI from './hooks/useWhoAmI';
 
 const Auth: React.FC = () => {
   const location = useLocation();
+  const whoAmI = useWhoAmI();
 
   useEffect(() => {
-    if (location.pathname !== '/auth/login' && location.pathname !== '/auth/registration') {
-      Axios.get(`${process.env.REACT_APP_KRATOS_SERVER}sessions/whoami`, {withCredentials: true})
-        .then((response: AxiosResponse<any>) => {
-          // if the session is valid then the hydra flow should continue
-          // With the response it is posible to validate if the user
-          // has validated addresses.
-
-          if (response.data.active) {
-            /* 
-              If you use localhost, use localhost in all places.
-              If you use 127.0.0.1, 127.0.0.1 in all places.
-              If there is a mix cookie will not work properly.
-
-              withCredentials has to be set to true in order
-              to set the cookies and send the cookies to the server*/
-            
-            Axios.get(`${process.env.REACT_APP_AUTH_SERVER_CSRF}`, {withCredentials: true})
-              .then(res => {
-
-                const csrfToken = document
-                  .cookie
-                  .split('; ')
-                  .find(cookie => cookie.startsWith('auth-csrf-token'))
-                  ?.split('=')[1];
-
-                /**
-                 * here withCredential is set to true to tell the browser to send
-                 * the session cookie required by flask in order to find the session
-                 * which stores the csrf token. remember that csrf tokens have a life 
-                 * of a session.
-                */
-                Axios.post(`${process.env.REACT_APP_AUTH_SERVER_LOGIN_CHALLENGE}`, {}, {
-                  withCredentials: true,
-                  headers: {'X-CSRFToken': csrfToken}
-                })
-                  .then(res => console.log(res));
-              });
-          }
-        }, (reason: any) => {
-          window.location.href = process.env.REACT_APP_KRATOS_SELF_SERVICE_LOGIN ?? '/';
-        });
+    if (
+        location.pathname !== '/auth/login' &&
+        location.pathname !== '/auth/registration' &&
+        whoAmI
+    ) {
+      if (whoAmI.data.active) {
+        /**
+         * here withCredential is set to true to tell the browser to send
+         * the session cookie required by flask in order to find the session
+         * which stores the csrf token. remember that csrf tokens have a life 
+         * of a session.
+        */
+        Axios.post(
+          `${process.env.REACT_APP_AUTH_SERVER_LOGIN_CHALLENGE}`, 
+          {},
+          {withCredentials: true}
+        )
+        .then(res => console.log(res));
+      }
     }
-  }, [location.pathname]);
+  }, [location.pathname, whoAmI]);
 
   return (
     <div className={style['auth-component']}>
@@ -76,6 +56,6 @@ const Auth: React.FC = () => {
       </Switch>
     </div>
   );
-}
+};
 
 export default Auth;
