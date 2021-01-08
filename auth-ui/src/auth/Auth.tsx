d51c47a6dd1dbd864de8c15b6e2c5428e893e05b
Edwin <edwin_m_paz@hotmail.com>
2021-01-08 16:40:19 -0600

    Add verify path in if condition and check if csrf token is setted

--- auth-ui/src/auth/Auth.tsx
+++ auth-ui/src/auth/Auth.tsx
@@ -1,21 +1,26 @@
 import React, { useEffect } from 'react';
-import {Route, Switch, useLocation} from 'react-router-dom'
+import {Route, Switch, useHistory, useLocation} from 'react-router-dom'
 import Login from './login/Login';
 import style from './Auth.module.scss';
 import Registration from './registration/Registration';
 import Verification from './verification/Verification';
 import Axios from 'axios';
 import useWhoAmI from './hooks/useWhoAmI';
+import useCsrfToken from '../shared/hooks/useCsrfToken';
 
 const Auth: React.FC = () => {
   const location = useLocation();
+  const history = useHistory()
   const whoAmI = useWhoAmI();
+  const isCsrfToken = useCsrfToken();
 
   useEffect(() => {
     if (
         location.pathname !== '/auth/login' &&
         location.pathname !== '/auth/registration' &&
-        whoAmI
+        location.pathname !== '/auth/verify' &&
+        whoAmI && 
+        isCsrfToken
     ) {
       if (whoAmI.data.active) {
         /**
@@ -32,12 +37,18 @@ const Auth: React.FC = () => {
         .then(res => console.log(res));
       }
     }
-  }, [location.pathname, whoAmI]);
+
+    if (location.pathname === '/auth/verify') {
+      history.push('/auth');
+    }
+  }, [location.pathname, whoAmI, history, isCsrfToken]);
 
   return (
     <div className={style['auth-component']}>
       {
-        location.pathname !== '/auth/login' && location.pathname !== '/auth/registration' ? (
+        location.pathname !== '/auth/login' &&
+        location.pathname !== '/auth/registration' &&
+        location.pathname !== '/auth/verify' ? (
           <div className={style['auth-message']}>
             Validating Session...
           </div>
