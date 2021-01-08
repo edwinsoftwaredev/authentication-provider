630c26d0b63a2ac7c4e7de134d7a61194425e387
Edwin <edwin_m_paz@hotmail.com>
2021-01-08 16:41:41 -0600

    Add condition in hook before checking whoami in kratos functionality

--- auth-ui/src/auth/hooks/useWhoAmI.tsx
+++ auth-ui/src/auth/hooks/useWhoAmI.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import Axios, { AxiosResponse } from 'axios';
+import { useLocation } from 'react-router-dom';
 
 interface IWhoAmI {
 
@@ -7,17 +8,24 @@ interface IWhoAmI {
 
 const useWhoAmI = (props?: IWhoAmI) => {
   const [response, setResponse] = useState<AxiosResponse>();
-
+  const location = useLocation();
+    
   useEffect(() => {
-    Axios.get(
-      `${process.env.REACT_APP_KRATOS_SERVER}sessions/whoami`,
-      {withCredentials: true}
-    ).then((res: AxiosResponse) => {
-      setResponse(res);
-    }, (reason: any) => {
-      window.location.href = process.env.REACT_APP_KRATOS_SELF_SERVICE_LOGIN ?? '/';
-    });
-  }, []);
+    if (
+      location.pathname !== '/auth/login' &&
+      location.pathname !== '/auth/registration' &&
+      location.pathname !== '/auth/verify'
+    ) {
+      Axios.get(
+        `${process.env.REACT_APP_KRATOS_SERVER}sessions/whoami`,
+        {withCredentials: true}
+      ).then((res: AxiosResponse) => {
+        setResponse(res);
+      }, (reason: any) => {
+        window.location.href = process.env.REACT_APP_KRATOS_SELF_SERVICE_LOGIN ?? '/';
+      });      
+    }
+  }, [location.pathname]);
 
   return response;
 }
