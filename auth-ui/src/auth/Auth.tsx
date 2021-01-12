29dfb96d7f260f1ac7f4a9c0e4769b32b0de481d
Edwin <edwin_m_paz@hotmail.com>
2021-01-11 18:26:08 -0600

    Add functionality to get login_challenge code

--- auth-ui/src/auth/Auth.tsx
+++ auth-ui/src/auth/Auth.tsx
@@ -5,21 +5,21 @@ import style from './Auth.module.scss';
 import Registration from './registration/Registration';
 import Verification from './verification/Verification';
 import Axios from 'axios';
-import useWhoAmI from './hooks/useWhoAmI';
 import useCsrfToken from '../shared/hooks/useCsrfToken';
+import useWhoAmI from './hooks/useWhoAmI';
 
 const Auth: React.FC = () => {
   const location = useLocation();
   const history = useHistory()
-  const whoAmI = useWhoAmI();
   const isCsrfToken = useCsrfToken();
+  const whoAmI = useWhoAmI();
 
   useEffect(() => {
     if (
         location.pathname !== '/auth/login' &&
         location.pathname !== '/auth/registration' &&
         location.pathname !== '/auth/verify' &&
-        whoAmI && 
+        whoAmI &&
         isCsrfToken
     ) {
       if (whoAmI.data.active) {
@@ -29,19 +29,21 @@ const Auth: React.FC = () => {
          * which stores the csrf token. remember that csrf tokens have a life 
          * of a session.
         */
+        const urlSearch = new URLSearchParams(location.search);
+        console.log(urlSearch.get('login_challenge'));
         Axios.post(
           `${process.env.REACT_APP_AUTH_SERVER_LOGIN_CHALLENGE}`, 
           {},
           {withCredentials: true}
         )
         .then(res => console.log(res));
-      }
+      } 
     }
 
     if (location.pathname === '/auth/verify') {
       history.push('/auth');
     }
-  }, [location.pathname, whoAmI, history, isCsrfToken]);
+  }, [location.pathname, history, whoAmI, isCsrfToken, location.search]);
 
   return (
     <div className={style['auth-component']}>
