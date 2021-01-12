33bc4ef5897e3e5310568aa41665a77d0be9c434
Edwin <edwin_m_paz@hotmail.com>
2021-01-11 18:32:49 -0600

    Add startAuthentication functionality at component mounting

---
+++ movie-polls-ui/src/App.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AuthService from './auth/auth-service';
+
+const startAuthentication = () => {
+  const authService = AuthService.getInstance();
+  authService.getUser().then(res => {
+    if (!res) {
+      authService.startAuthentication();
+    }
+
+    // save user
+  }, reason => {
+    console.log('Error retriving user.');
+  });
+};
+
+function App() {
+  
+  useEffect(() => {
+    startAuthentication();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
