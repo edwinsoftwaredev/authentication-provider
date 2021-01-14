90e66bc93ef8e577fcd1b3720a37d9f8d570115b
Edwin <edwin_m_paz@hotmail.com>
2021-01-14 17:56:00 -0600

    Implement consent challenge flow functions and update login challenge flow functions

--- auth-server/authserver/blueprints/auth.py
+++ auth-server/authserver/blueprints/auth.py
@@ -1,13 +1,13 @@
 import logging
 import os
 
-from flask import Blueprint, request, abort, redirect, jsonify
+from flask import Blueprint, request, abort
 from ory_hydra_client.api.admin_api import AdminApi
-from ory_hydra_client.models.login_request import LoginRequest
 from ory_hydra_client.rest import ApiException
 import ory_hydra_client
 import requests
 from requests.exceptions import Timeout
+from werkzeug.utils import redirect
 
 
 bp = Blueprint('auth', __name__, url_prefix='/api/auth')
@@ -49,8 +49,7 @@ def login_challenge():
             res_whoami = requests.get(f'{kratos_url}/sessions/whoami', cookies=request.cookies)
 
             if res_whoami.status_code == 401:
-                reject_login_request(api_instance, login_challenge)
-                abort(401)
+                return reject_login_request(api_instance, login_challenge)
             elif (res_whoami.json() and 
                 'active' in res_whoami.json() and
                 res_whoami.json()['active'] is True):
@@ -59,8 +58,7 @@ def login_challenge():
                 return accept_login_request(api_instance, login_challenge, subject)
 
             else:
-                reject_login_request(api_instance, login_challenge)
-                abort(401)
+                return reject_login_request(api_instance, login_challenge)
     except KeyError:
         logging.error('There was an error when trying to get value by key.')
         abort(400)
@@ -69,23 +67,129 @@ def login_challenge():
         abort(400)
 
 
+@bp.route('/consent-challenge', methods=['GET'])
+def get_consent_request():
+    def reject_consent_challenge(api_instance: AdminApi, consent_challenge: str):
+        body=ory_hydra_client.RejectRequest()
+        api_response=api_instance.reject_consent_request(consent_challenge, body=body)
+        return {'redirectUrl': api_response.redirect_to}
+
+    def accept_consent_challenge(api_instance: AdminApi, consent_challenge: str):
+        body = ory_hydra_client.AcceptConsentRequest()
+        api_response = api_instance.accept_consent_request(consent_challenge, body=body)
+        return {'redirectUrl': api_response.redirect_to}
+
+    configuration = ory_hydra_client.Configuration(
+        os.environ.get('HYDRA_ADMIN_SERVER')
+    )
+
+    try:
+        consent_challenge = request.args.get('consent_challenge')
+        if not consent_challenge:
+            abort(400)
+        
+        with ory_hydra_client.ApiClient(configuration) as api_client:
+            api_instance = ory_hydra_client.AdminApi(api_client)
+            api_response = api_instance.get_consent_request(consent_challenge)
+
+            if api_response.skip:
+                kratos_url=os.environ.get('KRATOS_SERVER')
+                res_whoami=requests.get(f'{kratos_url}/sessions/whoami', cookies=request.cookies)
+
+                if res_whoami.status_code == 401:
+                    return reject_consent_challenge(api_instance, consent_challenge)
+                elif (res_whoami.json() and 
+                    'active' in res_whoami.json() and
+                    res_whoami.json()['active'] is True):
+
+                    return accept_consent_challenge(api_instance, consent_challenge)
+                else:
+                    return reject_consent_challenge(api_instance, consent_challenge)
+
+            if not api_response.skip:
+                response = {
+                    'client': api_response.client.client_name,
+                    'challenge': api_response.challenge,
+                    'requested_scope': api_response.requested_scope,
+                    'user': api_response.subject
+                }
+                return response
+
+
+    except ValueError:
+        logging.error('It was not possible to retrive the value with the given key')
+    except Exception:
+        logging.error('There was an error in get_consent_request')
+
+    
+    return {'isTrue': True}
+
+
 @bp.route('/consent-challenge', methods=['POST'])
 def consent_challenge():
-    def reject_consent_challenge():
-        pass
+    def reject_consent_challenge(api_instance: AdminApi, consent_challenge: str):
+        body=ory_hydra_client.RejectRequest()
+        api_response=api_instance.reject_consent_request(consent_challenge, body=body)
+        return {'redirectUrl': api_response.redirect_to}
 
-    def accept_consent_challenge():
-        pass
+    def accept_consent_challenge(api_instance: AdminApi, consent_challenge: str):
+        body = ory_hydra_client.AcceptConsentRequest()
+        api_response = api_instance.accept_consent_request(consent_challenge, body=body)
+        return {'redirectUrl': api_response.redirect_to}
 
     req_json=request.get_json()
+    configuration=ory_hydra_client.Configuration(
+        os.environ.get('HYDRA_ADMIN_SERVER')
+    )
+
     try:
         consent_challenge=req_json['consentChallenge']
 
         if not consent_challenge:
             abort(400)
 
-        
-    except KeyError as key_error:
+        with ory_hydra_client.ApiClient(configuration) as api_client:
+            api_instance=ory_hydra_client.AdminApi(api_client)
+            kratos_url=os.environ.get('KRATOS_SERVER')
+            res_whoami=requests.get(f'{kratos_url}/sessions/whoami', cookies=request.cookies)
+
+            if res_whoami.status_code == 401:
+                return reject_consent_challenge(api_instance, consent_challenge)
+            elif (res_whoami.json() and 
+                'active' in res_whoami.json() and
+                res_whoami.json()['active'] is True):
+
+                return accept_consent_challenge(api_instance, consent_challenge)
+            else:
+                return reject_consent_challenge(api_instance, consent_challenge)
+
+    except KeyError:
+        logging.error(f'There was an error when getting value by key in request body')
+    except ApiException as api_exception:
+        logging.error(f'An ApiException occured: {api_exception.reason}')
+
+@bp.route('reject-consent-challenge', methods=['POST'])
+def rjct_consent_challenge(): 
+    def reject_consent_challenge(api_instance: AdminApi, consent_challenge: str):
+        body=ory_hydra_client.RejectRequest()
+        api_response=api_instance.reject_consent_request(consent_challenge, body=body)
+        return {'redirectUrl': api_response.redirect_to}
+
+    configuration=ory_hydra_client.Configuration(
+        os.environ.get('HYDRA_ADMIN_SERVER')
+    )
+
+    try:
+        consent_challenge=request.get_json()['consentChallenge']
+
+        if not consent_challenge:
+            abort(400)
+
+        with ory_hydra_client.ApiClient(configuration) as api_client:
+            api_instance = ory_hydra_client.AdminApi(api_client)
+            return reject_consent_challenge(api_instance, consent_challenge)
+
+    except KeyError:
         logging.error(f'There was an error when getting value by key in request body')
     except ApiException as api_exception:
         logging.error(f'An ApiException occured: {api_exception.reason}')
