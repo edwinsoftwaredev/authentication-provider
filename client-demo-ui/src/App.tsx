ee7342f728fe0a45db2c01d780d0415832ff4aae
Edwin <edwin_m_paz@hotmail.com>
2021-01-14 17:51:53 -0600

    Add Auth and AuthCodes components routes

--- client-demo-ui/src/App.tsx
+++ client-demo-ui/src/App.tsx
@@ -2,11 +2,13 @@ import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AuthService from './auth/auth-service';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Auth from './auth/Auth';
 
 const startAuthentication = () => {
   const authService = AuthService.getInstance();
   authService.getUser().then(res => {
-    if (!res) {
+    if (!res && window.location.pathname !== '/auth/codes') {
       authService.startAuthentication();
     }
 
@@ -24,20 +26,30 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          </Route>
+          <Route path='/auth'>
+            <Auth />
+          </Route>
+        </Switch>
+      </Router>
+
     </div>
   );
 }
