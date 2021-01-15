b1dbab9c149aa08740b330bff2fc70f18e1da6c0
Edwin <edwin_m_paz@hotmail.com>
2021-01-15 16:22:38 -0600

    Add Route to Logout component in client-demo-ui

--- client-demo-ui/src/auth/Auth.tsx
+++ client-demo-ui/src/auth/Auth.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import AuthCodes from './auth-codes/AuthCodes';
+import Logout from './logout/Logout';
 
 const Auth: React.FC = () => {
   return (
@@ -8,6 +9,9 @@ const Auth: React.FC = () => {
       <Switch>
         <Route exact path='/auth/codes'>
           <AuthCodes />
+        </Route>
+        <Route exact path='/auth/logout'>
+          <Logout />
         </Route>
       </Switch>
     </div>
