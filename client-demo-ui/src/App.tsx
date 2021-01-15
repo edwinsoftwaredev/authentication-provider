9fe6aa0ae72fc69817b2843adbe4abc53772f231
Edwin <edwin_m_paz@hotmail.com>
2021-01-15 16:22:05 -0600

    Add logout button in client-demo-ui

--- client-demo-ui/src/App.tsx
+++ client-demo-ui/src/App.tsx
@@ -1,28 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AuthService from './auth/auth-service';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Auth from './auth/Auth';
 
-const startAuthentication = () => {
+const startAuthentication = (setSigned: React.Dispatch<React.SetStateAction<boolean>>) => {
   const authService = AuthService.getInstance();
   authService.getUser().then(res => {
-    if (!res && window.location.pathname !== '/auth/codes') {
+    if (
+      !res && 
+      window.location.pathname !== '/auth/codes' &&
+      window.location.pathname !== '/auth/logout'
+    ) {
       authService.startAuthentication();
+    } else {
+      setSigned(true);
     }
-
     // save user
   }, reason => {
     console.log('Error retriving user.');
   });
 };
 
+const startSignOut = () => {
+  const authService = AuthService.getInstance();
+  authService.startSignOut();
+}
+
 function App() {
+  const [signed, setSigned] = useState(false);
   
   useEffect(() => {
-    startAuthentication();
+    startAuthentication(setSigned);
   }, []);
+
+  const handleSignOut = () => {
+    startSignOut();  
+  };
 
   return (
     <div className="App">
@@ -42,6 +57,9 @@ function App() {
               >
                 Learn React
               </a>
+              {
+                signed ? <button onClick={handleSignOut}>Sign Out</button> : null
+              }
             </header>
           </Route>
           <Route path='/auth'>
