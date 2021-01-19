d75df73e8af34ce5100f78af71a951163584477f
Edwin <edwin_m_paz@hotmail.com>
2021-01-19 15:58:58 -0600

    Remove logo import

--- auth-ui/src/App.tsx
+++ auth-ui/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import AppStyle from './App.module.scss';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Auth from '../src/auth/Auth';
