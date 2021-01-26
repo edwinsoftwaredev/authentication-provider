aea46090d95dfc7e0384525560afa7b19a3d07a4
Edwin <edwin_m_paz@hotmail.com>
2021-01-26 11:06:47 -0600

    Add conditional rendering based on data variable value in Consent component

--- auth-ui/src/auth/consent/Consent.tsx
+++ auth-ui/src/auth/consent/Consent.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 import Button from '../../shared/button/button';
 import style from './Consent.module.scss';
 import Axios from 'axios';
@@ -7,75 +7,90 @@ import Axios from 'axios';
 const Consent: React.FC = () => {
   const location = useLocation();
   const data: any = location.state;
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!data) {
+      history.push('/');
+    }
+  }, [data, history]);
 
   const handleAllowClick = () => {
-    Axios.post(`${process.env.REACT_APP_AUTH_SERVER_CONSENT_CHALLENGE}`,
-    {
-      consentChallenge: data.challenge
-    }, {withCredentials: true})
-    .then(res => {
-      window.location.href = res.data.redirectUrl;
-    })
+    if (data) {
+      Axios.post(`${process.env.REACT_APP_AUTH_SERVER_CONSENT_CHALLENGE}`,
+      {
+        consentChallenge: data.challenge
+      }, {withCredentials: true})
+      .then(res => {
+        window.location.href = res.data.redirectUrl;
+      });
+    }
   };
 
   const handleCancelClick = () => {
-    Axios.post(`${process.env.REACT_APP_AUTH_SERVER_REJECT_CONSENT_CHALLENGE}`,
-    {
-      consentChallenge: data.challenge
-    }, {withCredentials: true})
-    .then(res => {
-      window.location.href = res.data.redirectUrl;
-    });
+    if (data) {
+      Axios.post(`${process.env.REACT_APP_AUTH_SERVER_REJECT_CONSENT_CHALLENGE}`,
+      {
+        consentChallenge: data.challenge
+      }, {withCredentials: true})
+      .then(res => {
+        window.location.href = res.data.redirectUrl;
+      });
+    }
   };
 
   return (
     <div className={style['consent-container']}>
-      <div className={style['consent']}>
-        <div className={style['header']}>
-          <span className={style['client-name']}>{data.client} </span>
-          wants to access your account
-        </div>
-        <div className={style['body']}>
-          <div>
-            This will allow
-            <span className={style['client-name']}> {data.client} </span>
-            to access your account with the following scopes:
+      {
+        data ? (
+          <div className={style['consent']}>
+            <div className={style['header']}>
+              <span className={style['client-name']}>{data ? data.client : ''} </span>
+              wants to access your account
+            </div>
+            <div className={style['body']}>
+              <div>
+                This will allow
+                <span className={style['client-name']}> {data ? data.client : ''} </span>
+                to access your account with the following scopes:
+              </div>
+              <div className={style['scopes']}>
+                <ul>
+                  {
+                    data ? data.requested_scope.map((item: any, index: any) => {
+                      return <li key={index}>{item}</li>
+                    }) : null
+                  }
+                </ul>
+              </div>
+            </div>
+            <div className={style['important-note']}>
+              Make sure you trust 
+              <span className={style['client-name']}> {data ? data.client : ''} </span>
+              <div className={style['note']}>
+                  You may be sharing sensitive info with this site or app.
+              </div>
+            </div>
+            <div className={style['footer']}>
+              <div className={style['buttons']}>
+                  <Button
+                    classType='default'
+                    type='button'
+                    text='Cancel' 
+                    onClick={handleCancelClick}
+                  />
+                  <div></div>
+                  <Button
+                    classType='contained'
+                    type='button'
+                    text='Allow' 
+                    onClick={handleAllowClick}
+                  />
+              </div>
+            </div>
           </div>
-          <div className={style['scopes']}>
-            <ul>
-              {
-                data.requested_scope.map((item: any, index: any) => {
-                  return <li key={index}>{item}</li>
-                })
-              }
-            </ul>
-          </div>
-        </div>
-        <div className={style['important-note']}>
-          Make sure you trust 
-          <span className={style['client-name']}> {data.client} </span>
-          <div className={style['note']}>
-              You may be sharing sensitive info with this site or app.
-          </div>
-        </div>
-        <div className={style['footer']}>
-          <div className={style['buttons']}>
-              <Button
-                classType='default'
-                type='button'
-                text='Cancel' 
-                onClick={handleCancelClick}
-              />
-              <div></div>
-              <Button
-                classType='contained'
-                type='button'
-                text='Allow' 
-                onClick={handleAllowClick}
-              />
-          </div>
-        </div>
-      </div>
+        ) : null
+      }
     </div>
   )
 }
