bc898e3607efebdaad6962b6baa24fcae5176256
Edwin <edwin_m_paz@hotmail.com>
2021-01-28 17:08:46 -0600

    Register blueprint

--- auth-server/authserver/blueprints/__init__.py
+++ auth-server/authserver/blueprints/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
-from . import auth, csrf_token
+from . import auth, csrf_token, clients
 
 def init_app(app: Flask):
     csrf = CSRFProtect(app)
     app.register_blueprint(auth.bp)
     app.register_blueprint(csrf_token.bp)
+    app.register_blueprint(clients.bp)
 
     # This bp does not have csrf protection.
     # It is not required because it generates
